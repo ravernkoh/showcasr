cb89166c081e8d91d3268d61bed6df54874aae19
Ravern Koh <ravernkoh@gmail.com>
2019-05-18 10:47:17 +0800

    Create the query syntax and database call

--- backend/src/firebase/db/projects.js
+++ backend/src/firebase/db/projects.js
@@ -1,12 +1,42 @@
 const {convertTimestampsToDates} = require('./util');
 
-const all = db => async () => {
+const all = db => async query => {
   const projects = [];
+
   const snapshot = await db.collection('projects').get();
   snapshot.forEach(doc => {
     const project = convertTimestampsToDates(doc.data());
+
+    // Filter based on the different `query` properties.
+    if (query.tags) {
+      for (const tag of query.tags) {
+        if (!project.tags.includes(tag)) {
+          return;
+        }
+      }
+    }
+    if (query.course) {
+      for (const course of query.course) {
+        let found = false;
+        if (project.course === course) {
+          found = true;
+          break;
+        }
+        if (!found) {
+          return;
+        }
+      }
+    }
+    if (query.title && !project.title.includes(query.title)) {
+      return;
+    }
+    if (query.description && !project.description.includes(query.description)) {
+      return;
+    }
+
     projects.push(project);
   });
+
   return projects;
 };
 
