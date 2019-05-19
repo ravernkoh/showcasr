689544c0e8e5b812c2fb5055a48422a44ec380fa
Ravern Koh <ravernkoh@gmail.com>
2019-05-20 02:18:40 +0800

    Create screen states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,57 @@
 import React, {Component} from 'react';
 
+import firebase from 'firebase/app';
+
 import Display from './components/Display';
+import Loading from './components/Loading';
+import Login from './components/Login';
 
 import './App.css';
 
+const SCREEN_LOADING = 'LOADING';
+const SCREEN_LOGIN = 'LOGIN';
+const SCREEN_DISPLAY = 'DISPLAY';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentScreen: SCREEN_LOADING,
+    };
+  }
+
+  componentDidMount() {
+    firebase
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then(() => {
+        this.registerFirebaseAuthStateObserver();
+      })
+      .catch(console.error);
+  }
+
+  registerFirebaseAuthStateObserver() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({currentScreen: SCREEN_DISPLAY});
+      } else {
+        this.setState({currentScreen: SCREEN_LOGIN});
+      }
+    });
+  }
+
   render() {
-    return <Display />;
+    switch (this.state.currentScreen) {
+      case SCREEN_LOADING:
+        return <Loading />;
+      case SCREEN_DISPLAY:
+        return <Display />;
+      case SCREEN_LOGIN:
+        return <Login />;
+      default:
+        break;
+    }
   }
 }
 
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+  render() {
+    return <div>Hello, world!</div>;
+  }
+}
+
+export default Login;
